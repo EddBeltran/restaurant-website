70c94072430b20f3dc5a8d931083dfcc7874e404
Eduardo Beltran <edd.bng@gmail.com>
2022-01-28 23:19:26 -0600

    Se agregó la seccion de feedback

---
+++ js/feedbackSlider.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".feedback", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    //autoplay: {
+    //  delay: 3500,
+    //  disableOnInteraction: false,
+    //},
+    breakpoints: {
+      425: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+});
