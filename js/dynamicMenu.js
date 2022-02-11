de422a5c8ae35a8e27e62b47ed23f66792696e76
Eduardo Beltran <edd.bng@gmail.com>
2022-02-10 23:42:12 -0600

    Se agregó la seccion hero

--- js/dynamicMenu.js
+++ js/dynamicMenu.js
@@ -1,14 +1,16 @@
 const buttons = document.querySelectorAll('.menu-category');
+const btnContainer = document.getElementById('myBtnContainer')
 const cards = document.querySelectorAll('.menu__container-card');
 
+
 buttons.forEach((button) => {
-    if (button.classList.contains('active')){
-        console.log("bro")
-        button.classList.remove('active')
-    }
     button.addEventListener('click', (e) => {
+        buttons.forEach((element) => {
+            element.classList.remove("active");
+        });
         button.classList.add('active');
         const btnCategory = button.getAttribute('data-category');
+        
         cards.forEach((card)=>{
             const cardCategory = card.getAttribute('data-category');
             if (btnCategory == cardCategory) {
