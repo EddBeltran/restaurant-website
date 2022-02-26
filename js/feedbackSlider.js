9c66c74dedad1903beba56435285d077478321da
Eduardo Beltran <edd.bng@gmail.com>
2022-02-26 00:53:09 -0600

    se agregó pdf y funcion a feeback form

--- js/feedbackSlider.js
+++ js/feedbackSlider.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".feedback", {
+var swiperFeedback = new Swiper(".feedback", {
     slidesPerView: 1,
     spaceBetween: 40,
     loop: true,
@@ -29,3 +29,55 @@ var swiper = new Swiper(".feedback", {
       },
     },
 });
+
+
+const btnFeedback = document.querySelectorAll('.modal__content__emojis__button');
+const btnPublish = document.getElementById('publish');
+const inputName = document.getElementById('input-name');
+const textareaFeedback = document.getElementById('textarea-feedback');
+const modalBox = document.querySelector(".modal");
+const pWarning = document.getElementById('p-warning');
+
+
+
+
+let emoji = "feliz";
+btnFeedback.forEach((button) => {
+    button.addEventListener('click', ()=>{
+      clearEmojiStatus();
+      button.classList.add('active');     
+        emoji = button.id;
+    });
+});
+
+function clearEmojiStatus(){
+  btnFeedback.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
+
+btnPublish.addEventListener('click', ()=>{
+  if (textareaFeedback.value.length > 0 && inputName.value.length > 0){
+    var input = document.createElement("blockquote");
+    input.className = 'swiper-slide feedback__slide';
+    input.innerHTML = (
+        '<div class="feedback__slide__quote">' + textareaFeedback.value + '</div>'
+        + '<div class="feedback__slide__footer">'
+            +'<p>'+ inputName.value+'</p>'
+            +`<img src="images`+'/'+`cara_`+emoji+`.svg"/>`
+        +'</div>'
+    );
+    swiperFeedback.addSlide(1, input);
+    swiperFeedback.slideTo(2, 500, true);
+    modalBox.style.display = "none";
+    pWarning.style.display = 'none';
+    textareaFeedback.value = '';
+    inputName.value = '';
+    clearEmojiStatus();
+    
+  } else {
+    pWarning.style.display = 'block';
+  }
+});
+
+
